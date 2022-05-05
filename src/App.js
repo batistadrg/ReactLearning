820634f47504a82b975c5bfabe70905a11d4835b
batistadrg <igor.batista0802@outlook.com>
2022-05-05 17:42:42 -0300

    useState com class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.handleClick = this.handleClick.bind(this)
+    //bind necessario para uma funcao usar o state
+
+    this.state = {
+
+      reverse: false
+    }
+  }
+
+
+  handleClick(){
+    const {reverse} = this.state;
+    this.setState({reverse: !reverse})
+  }
+
+  render(){
+    const {reverse} = this.state
+    const reverseClass = reverse ? 'reverse' : '';
+
+    /*reverseClass eh uma variavel que recebe a seguinte condicional:
+      se reverse for verdadeiro ? retorne 'verse' : se nao, retorne ''
+    */
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+          <button 
+          type="button"
+          onClick = {this.handleClick}>
+            Reverse {reverseClass}
+          </button>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
