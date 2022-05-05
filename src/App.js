f2650da7508cb83013101b564aebb14f5363f1e0
batistadrg <igor.batista0802@outlook.com>
2022-05-05 17:56:18 -0300

    useState com funcao

--- src/App.js
+++ src/App.js
@@ -1,46 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react';
+import { useState } from 'react';
 
-class App extends Component {
-  constructor(props){
-    super(props)
 
-    this.handleClick = this.handleClick.bind(this)
-    //bind necessario para uma funcao usar o state
 
-    this.state = {
+function App () {
+  const [reverse, setReverse] = useState(false);
+  const [contador, setContador] = useState(0)
+  const reverseClass = reverse ? 'reverse' : ''
 
-      reverse: false
-    }
+  const handleClick = () => {
+    setContador(contador+1)
+    setReverse(!reverse)
   }
 
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+        <h1>Contador: {contador}</h1>
+        <button 
+        type="button"
+        onClick = {handleClick}>
+          Reverse {reverseClass}
+        </button>
+      </header>
+    </div>
+  )
 
-  handleClick(){
-    const {reverse} = this.state;
-    this.setState({reverse: !reverse})
-  }
-
-  render(){
-    const {reverse} = this.state
-    const reverseClass = reverse ? 'reverse' : '';
-
-    /*reverseClass eh uma variavel que recebe a seguinte condicional:
-      se reverse for verdadeiro ? retorne 'verse' : se nao, retorne ''
-    */
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
-          <button 
-          type="button"
-          onClick = {this.handleClick}>
-            Reverse {reverseClass}
-          </button>
-        </header>
-      </div>
-    )
-  }
 }
 
 export default App;
