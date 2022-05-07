f7aaa7b9fd65c893b6d1d92ed7093723c288d33a
batistadrg <igor.batista0802@outlook.com>
2022-05-07 11:52:00 -0300

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,66 +1,40 @@
 import logo from './logo.svg';
+import P from "prop-types"
 import './App.css';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 
-const eventFn = () => {
-  console.log("h1 clicado")
 
 
-}
+// React.memo serve para memorizar o componente caso ele nao mude, caso ele mude, ela muda tambem
 
+const Button = React.memo(function Button({incrementButton}) {
+  console.log("Filho renderizou")
+  return <button onClick={() => incrementButton(10)}>+</button>
+}) // component chamado Button
+
+
+Button.propTypes = {
+  incrementButton: P.func,
+};
 
 
 function App () {
   const [counter, setCounter] = useState(0)
 
+  // Serve para recriar a funcao apenas se a dependencia mudar
+  const incrementCounter = useCallback((num) => {
+    setCounter((c) => c + num)
+    // funcao basica de callback
 
-  //componenDidUpdate - executa toda vez que o component atualiza
+  }, [])
 
-  useEffect(()=> {
-    console.log("componentDidUpdate")
-
-  })
-
-
-  //componenDidMount - executa 1x
-
-  useEffect(()=> {
-    console.log("componentDidMount")
-
-  }, []) // como passamos um array de dependencias vazio, ele executa apenas 1x
-
-
-  //Com dependencia - executa quando a dependencia mudar
-
-  useEffect(()=> {
-    console.log("Contador mudou para",counter)
-
-  }, [counter]) // repare que passamos counter como dependencia
-
-
-
-
-
-    useEffect(()=> {
-      document.querySelector("h1")?.addEventListener("click", eventFn); //JS puro
-      // ? Encadeamento opcional. Caso haja o componente h1, o que esta depois da virgula sera executado. Caso nao, nao sera.
-    
-
-      // componentWillUnmount - Serve para limpar os lixos da pagina
-    return () => {
-      document.querySelector("h1")?.removeEventListener("click", eventFn);
-    }
-    
-    }, []) 
-
-
-//lifecyclemethotds
+  console.log("Pai renderizou")
 
   return (
     <div className="App">
       <h1>Contador: {counter}</h1>
-      <button onClick={()=> setCounter(counter+1)}>+</button>
+      <Button incrementButton={incrementCounter}></Button>
     </div>
   )
 
